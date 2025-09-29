46ed1bf2378b3fe535b4352fac556432277921f3
Somil Agrawal <somilagrawal459@gmail.com>
2025-09-29 20:01:53 +0530

    Add files via upload

---
+++ structure as parameter/by address/ba.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct rectangle{
+    int length;
+    int breath;
+};
+
+int area(struct rectangle &r){
+    r.length++;
+    return r.length*r.breath;
+    
+}
+
+int main()
+{
+    struct rectangle r={10,5};
+    cout<<"The area is "<<area(r)<<endl;
+    cout<<r.length<<endl<<r.breath<<endl;
+
+    return 0;
+}
